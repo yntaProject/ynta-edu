d45a974dd3d882e0ded79020724b06eb2795e1c1
erzhigitwx <erjik.red@gmail.com>
2023-10-10 16:00:49 +0600

    change hook

--- src/shared/API/hooks/useFetchData.ts
+++ src/shared/API/hooks/useFetchData.ts
@@ -5,7 +5,7 @@ import { db } from "../../../firebase";
 type UseFetchDataReturn<T> = [T[], boolean];
 
 export function useFetchData<T extends {
-  createdAt: Date
+  createdAt: any
 }>(path: string, addCreatedAt?: boolean): UseFetchDataReturn<T> {
   const [data, setData] = useState<T[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -18,7 +18,6 @@ export function useFetchData<T extends {
         querySnapshot.forEach((doc) => {
           const docData = doc.data();
           if (addCreatedAt) {
-            // @ts-ignore
             const timestamp = doc._document.createTime.timestamp;
             const milliseconds = (timestamp.seconds * 1000) + (timestamp.nanoseconds / 1000000);
             newData.push({ ...docData, id: doc.id, createdAt: new Date(milliseconds) } as unknown as T);
